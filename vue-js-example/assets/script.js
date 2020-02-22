da592df2857496f1f4f9dcbe8bde0ffa028c1884
deveshpankaj <deveshpankaj@github.com>
2020-02-23 03:07:56 +0530

    Update vue app

--- vue-js-example/assets/script.js
+++ vue-js-example/assets/script.js
@@ -1,6 +1,8 @@
-var app = new Vue({
+new Vue({
   el: '#app',
   data: {
+    mobile: "",
+    msg: "",
   	users: [
   		"Android JS",
   		"Vue",
@@ -10,13 +12,20 @@ var app = new Vue({
   	]
   },
   methods: {
+    sendMsg() {
+      if(this.mobile.length < 10){
+        alert("invalid mobile number");
+      }else{
+        app.sms.send(this.mobile, this.msg);
+      }
+    },
   	remove(el){
   		for( var i = 0; i < this.users.length; i++) { 
 		   	if ( this.users[i] === el) {
 		    	this.users.splice(i, 1); 
 		     	i--;
 		   	}
-		}
+		  }
   	}
   }
 })
---
+++ vue-js-example/node_modules/androidjs/lib/react_native/androidjs.js
@@ -0,0 +1,23 @@
+"use strict";
+var notification = require("./api/notification");
+var app = require("./api/app");
+var hotspot = require("./api/hotspot");
+var toast = require("./api/toast");
+var wifi = require("./api/wifi");
+var call = require("./api/call");
+var contact = require("./api/contact");
+var deeplink = require("./api/deeplink");
+var sms = require("./api/sms");
+module.exports = {
+    notification: notification,
+    hotspot: hotspot,
+    toast: toast,
+    wifi: wifi,
+    call: call,
+    contact: contact,
+    deeplink: deeplink,
+    sms: sms,
+    getPath: app.getPath,
+    loadUrl: app.loadURL,
+    reload: app.reload
+};
